599fb3510382b7f4e35082e0b4f5f66ae8ea276b
Isaac Smith <isaacsmith215@aol.com>
2020-03-10 19:42:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 def countdown(number = 10)
-  while number > 0
+  until number > 0
     number -= 1
     sleep(1)
     puts "#{number} SECOND(S)!"
   end
-  if number == 0
+  while number == 0
     puts "HAPPY NEW YEAR!"
     break
   end
